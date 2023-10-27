aead67754bf744f881ef46424438fa73c1db0b7c
jo22i <99878853+jo22i@users.noreply.github.com>
2023-10-28 00:39:04 +0300

    Переработана логика дэка. Теперь есть указатели на начало и конец и парвильная логика удаления элемента из дэка

--- ejudje_M1T2.py
+++ ejudje_M1T2.py
@@ -1,145 +1,186 @@
 class array_deque:
+    # Инициализация первоначального размера дэка для удобства детектирования
+    # попыток его повторной инициализации
     capacity = 0
 
-    def __init__(self, size):
-        if(size <= 0):
+    def __init__(self, size: int):
+        # Проверка длины на неотрицательность и нуль
+        if (size <= 0):
             print("error")
             return
         
+        # Установка размера дэка
         self.mass = [""]*size
+        # Установка переменной с размером дэка
         self.capacity = size
+        # Установка переменной с кол-вом записанных ячеек
         self.length = 0
-
+        # Установка указателей на первый и последний элемент дэка
+        # Так как дэк новый, то указатели одинаковые и указывают на единственный
+        # пока ещё не появившийся первый элемент
+        # В данном случае было решено поставить первый элемент в начало массива
         self.head = self.tail = 0
 
     def print(self):
-        if(self.length == 0):
+        # Проверка на пустоту и возможный вызов функции при отсутствии инициализации дэка
+        if (self.length == 0):
             print("empty")
             return
-        elif(self.capacity == 0):
+        elif (self.capacity == 0):
             print("error")
             return
 
+        # Если элемент всего один, то он и печатается
         if (self.length == 1):
             print(self.mass[self.head])
             return
 
+        # Иначе перебираем все элементы дека, начиная с начального указателя 
+        # и до последнего доступного элемента
         answer = ""
         index = self.head
-        while (index != self.tail + 1):
+        while (index != self.tail):
+            # Циклический переход из конца массива в начало
             if (index == len(self.mass)):
                 index = 0
-            answer += (self.mass[index] + " ") if (self.mass[index] != "") else ""
+            answer += self.mass[index] + " "
             index += 1
+        else:
+            # Перехват последнего элемента
+            answer += self.mass[index]
         
+        # Вывод всех элементов
         print(answer.rstrip())
 
-    def pushf(self, elem):
-        if(self.capacity == 0):
+    def pushf(self, elem: str):
+        # Проверка на переполнение и возможный вызов функции при отсутствии инициализации дэка
+        if (self.capacity == 0):
             print("error")
             return
-        elif(self.capacity == self.length):
+        elif (self.capacity == self.length):
             print("overflow")
             return
         
+        # Если дэк пуст, то добавляемый элемент - первый, ставится в начало
         if (self.length == 0):
             self.mass[self.head] = elem
             self.length += 1
             return
         
+        # Иначе ставим его первым элементом в списке и переcтавляем на него указатель head
         self.length += 1
-
         self.head = (self.capacity - 1) if (self.head == 0) else (self.head - 1)
-        
         self.mass[self.head] = elem
 
-    def pushb(self, elem):
-        if(self.capacity == 0):
+    def pushb(self, elem: str):
+        # Проверка на переполнение и возможный вызов функции при отсутствии инициализации дэка
+        if (self.capacity == 0):
             print("error")
             return
-        elif(self.capacity == self.length):
+        elif (self.capacity == self.length):
             print("overflow")
             return
         
+        # Если дэк пуст, то добавляемый элемент - первый, ставится в начало
         if (self.length == 0):
             self.mass[self.head] = elem
             self.length += 1
             return
 
+        # Иначе ставим его на следующее место после tail и меняем указатель tail на него
         self.length += 1
-
         self.tail = (0) if (self.tail == self.capacity - 1) else (self.tail + 1)
-
         self.mass[self.tail] = elem
         
     def popb(self):
-        if(self.capacity == 0):
+        # Проверка на пустоту и возможный вызов функции при отсутствии инициализации дэка
+        if (self.capacity == 0):
             print("error")
             return
-        elif(self.length == 0):
+        elif (self.length == 0):
             print("underflow")
             return
         
+        # Уменьшаем длину списка на 1 элемент
         self.length -= 1
         
+        # Если там был всего один элемент - его и выводим, он на указателях head и tail
         if (self.length == 0):
             print(self.mass[self.head])
             return
 
-        print(self.mass[self.tail])
-
+        # Иначе берём его по указателю tail и сдвигаем указатель
+        answer, self.mass[self.tail] = self.mass[self.tail], ""
         self.tail = (self.capacity - 1) if (self.tail == 0) else (self.tail - 1)
 
+        # Возврат элемента
+        return answer
+
     def popf(self):
-        if(self.capacity == 0):
+        # Проверка на пустоту и возможный вызов функции при отсутствии инициализации дэка
+        if (self.capacity == 0):
             print("error")
             return
-        elif(self.length == 0):
+        elif (self.length == 0):
             print("underflow")
             return
-
+        
+        # Уменьшаем длину списка на 1 элемент
         self.length -= 1
 
+        # Если там был всего один элемент - его и выводим, он на указателях head и tail
         if (self.length == 0):
             print(self.mass[self.head])
             return
         
-        print(self.mass[self.head])
+        # Иначе берём его по указателю head и сдвигаем указатель
+        answer, self.mass[self.head] = self.mass[self.head], ""
+        self.head = (0) if (self.head == self.capacity - 1) else (self.head + 1)
 
-        self.head += 1
-        if (self.head == self.capacity): self.head = 0
+        # Возврат полученного элемента
+        return answer
 
+# Инициализация пустого дэка и переменной для получения команд из командной строки
 Deque = array_deque
-
 line = ""
 
-while(True):
+# Цикл обработки команд из командной строки
+while (True):
     try:
         line = input()
-        if(line == ""):
+        # Пропуск пустых строк
+        if (line == ""):
             continue
 
+        # Получение списка команд с помощью разбиения строки по разделительному пробелу
         commands = line.split(" ", 1)
 
-        if(len(commands) == 1):
+        # Проверка на кол-во команд
+        # Одна строка - 3 возможные команды: popf, popb, print
+        # Иначе получается 2 строки и соответственно 3 другие команды: set_size, pushf, pushb
+        # Если есть несоответствие 3 вышеперечисленным командам - выводим ошибку
+        if (len(commands) == 1):
             match commands[0]:
                 case "print":
                     Deque.print()
                     continue
                 case "popf":
-                    Deque.popf()
+                    print(Deque.popf())
                     continue
                 case "popb":
-                    Deque.popb()
+                    print(Deque.popb())
                     continue
                 case _:
                     print("error")
                     continue
+        
 
-        if(commands[1].find(" ") != -1 or commands[1] == ""):
+        # Если нашли лишние пробелы во второй строке для команд - значит он лишний
+        # и необходимо вывести сообщение об ошибке
+        if (commands[1].find(" ") != -1 or commands[1] == ""):
             print("error")
             continue
-        
+         
         try:
             match commands[0]:
                 case "set_size":
@@ -154,13 +195,16 @@ while(True):
                 case _:
                     print("error")
             continue
+        # Если при получении команды set_size во втором агрументе было не целое число,
+        # то вызывается ValueError, который мы ловим и выводим собщение об ошибке
         except ValueError:
             print("error")
             continue
         except:
             print("error")
             continue
-
+    # Данный тип ошибки возникнет при окончании ввода с консоли, 
+    # который означает завершение программы
     except EOFError:
         break
     except:
