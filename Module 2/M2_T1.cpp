e2367712e675c70737daed96a6c06460907e5638
jo22i <99878853+jo22i@users.noreply.github.com>
2023-11-08 01:34:43 +0300

    Minor fixes

--- Module 2/M2_T1.cpp
+++ Module 2/M2_T1.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <deque>
 
 class SplayTree
 {
@@ -8,11 +9,12 @@ class SplayTree
     SplayTree* parent;
     SplayTree* left;
     SplayTree* right;
+    bool is_root;
 
 public:
-    SplayTree() : key(NULL), value(NULL), parent(nullptr), left(nullptr), right(nullptr) {}
+    SplayTree() : key(NULL), value(NULL), parent(nullptr), left(nullptr), right(nullptr), is_root(true) {}
 
-    SplayTree(const uint64_t& key, const std::string& value) : parent(nullptr), left(nullptr), right(nullptr)
+    SplayTree(const uint64_t& key, const std::string& value) : parent(nullptr), left(nullptr), right(nullptr), is_root(true)
     {
         this->key = key;
         this->value = value;
@@ -27,7 +29,69 @@ public:
 
     void add(const uint64_t& new_key, const std::string& new_value)
     {
-        return;
+        if (this->left == nullptr && this->right == nullptr)
+        {
+            if (this->key == new_key)
+            {
+                std::cout << "error" << std::endl;
+                return;
+            }
+            else if (this->key > new_key)
+            {
+                this->left = new SplayTree(new_key, new_value);
+                return;
+            }
+            else
+            {
+                this->right = new SplayTree(new_key, new_value);
+                return;
+            }
+        }
+
+        std::deque<SplayTree*> stack = {};
+        stack.push_front(this->right);
+        stack.push_front(this->left);
+
+        while (stack.size() > 0)
+        {
+            SplayTree* current_elem = stack.front();
+            stack.pop_front();
+
+            if (current_elem->key == new_key)
+            {
+                std::cout << "error" << std::endl;
+                return;
+            }
+
+            if (current_elem->key > new_key)
+            {
+                if (current_elem->right == nullptr)
+                {
+                    current_elem->right = new SplayTree(new_key, new_value);
+                    stack.clear();
+                    return;
+                }
+                else
+                {
+                    stack.push_front(current_elem->right);
+                    stack.clear();
+                    continue;
+                }
+            }
+            else
+            {
+                if (current_elem->left == nullptr)
+                {
+                    current_elem->left = new SplayTree(new_key, new_value);
+                    return;
+                }
+                else
+                {
+                    stack.push_front(current_elem->left);
+                    continue;
+                }
+            }
+        }
     }
 
     void set(const uint64_t& key, const std::string& new_value)
