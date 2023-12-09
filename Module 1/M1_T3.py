51335208bcbe7228f71bc69969fafbc8e47b3d47
jo22i <99878853+jo22i@users.noreply.github.com>
2023-12-09 17:29:26 +0300

    EEEEEE

--- Module 1/M1_T3.py
+++ Module 1/M1_T3.py
@@ -1,21 +1,29 @@
 import sys
-from collections import defaultdict
 
-def SearchPaths(dep_lib: str, current_path: list):
-    current_path = [dep_lib] + current_path 
-    if dep_lib in dependencies:
-        print(' '.join(current_path))
+def SearchPaths(item, lib_path, libs_visited, grath, libs_printed):
+    global dependencies
 
-    if dep_lib in libs:
-        for lib in libs[dep_lib]:
-            if lib not in current_path:
-                SearchPaths(lib, current_path)
+    if item not in libs_visited:
+        lib_path.append(item)
+        libs_visited.add(item)
+
+        if item in grath:
+            for next_item in grath[item]:
+                SearchPaths(next_item, lib_path, libs_visited, grath, libs_printed)
+
+        if item in dependencies and tuple(lib_path) not in libs_printed:
+            print(" ".join(reversed(lib_path)))
+            libs_printed.add(tuple(lib_path))
+
+        if lib_path:
+            libs_visited.remove(item)
+            lib_path.pop()
 
 
 vulnerables = set(input().split())
 dependencies = set(input().split())
-
-libs = defaultdict(set)
+lib_way = []
+libs = {}
 
 for line in sys.stdin:
     line = line.strip('\n')
@@ -23,7 +31,8 @@ for line in sys.stdin:
         lib_name, *lib_depends = line.split()
         
         for elem in lib_depends:
-            libs[elem].add(lib_name)
+            libs.setdefault(elem, []).append(lib_name)
 
+printed_paths = set()
 for elem in vulnerables:
-    SearchPaths(elem, [])
+    SearchPaths(elem, lib_way, set(), libs, printed_paths)
